68172c44878db90f04c0b87959bb7a8b02227a4f
David Taylor <dmtaylor2011@gmail.com>
2019-02-08 19:13:49 -0800

    Add skel roll_list class

---
+++ src/wh40k8edutils/roll_list.py
@@ -0,0 +1,30 @@
+'''
+
+A list of rolls representing a batch of rolls
+
+@author: David
+'''
+
+from typing import List
+from wh40k8edutils.dieroll import DieRoll
+
+class RollList:
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, num_rolls: int, passvalue: int,
+                 reroll_values: List[int] = None, mod: int = 0 ):
+        '''
+        Constructor
+        '''
+        self.num_rolls = num_rolls
+        self.passvalue = passvalue
+        self.reroll_values = reroll_values
+        self.mod = mod
+        
+        self.rolls = []
+        for _ in range(self.num_rolls):
+            self.rolls.append(DieRoll())
+        
