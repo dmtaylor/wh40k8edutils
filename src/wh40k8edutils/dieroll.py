1d63183263ec03f981bb0e686b8956041eab34d5
David Taylor <dmtaylor2011@gmail.com>
2019-02-09 01:16:35 -0800

    Add functionality to rolling
    Worked on the logic for rolling primitives. Added methods, did some
    minor refactoring

--- src/wh40k8edutils/dieroll.py
+++ src/wh40k8edutils/dieroll.py
@@ -2,30 +2,49 @@
 A class to represent a single die roll, keeping track of re-rolls and
 modified values
 
-@author: David
+@author: David Taylor
 '''
 
 import random
 
 class DieRoll:
-    '''
-    classdocs
+    ''' Class representing an individual die roll
+        This class represents a single die roll, and tracks all the required
+        information.
+        
+        Class variables:
+            d6_values: faces on a d6
+            d3_values: faces on a d3
+        
+        Instance variables:
+            isd3: is a roll of a d3 rather than the typical d6
+            orig_value: unmodified value of roll
+            rerolled_value: value after any rerolls
+            mod_value: value after all modifiers taken into account
+            rerolled: Has this die been re-rolled? A die can only be rerolled
+                    once.
+                    
     '''
     
     d6_values = [1,2,3,4,5,6]
     d3_values = [1,2,3]
 
-    def __init__(self):
+    def __init__(self, isd3: bool = False):
         '''
         Constructor
         '''
-        self.orig_value = random.choice(self.d6_values)
+        self.isd3 = isd3
+        if isd3:
+            self.orig_value = random.choice(self.d3_values)
+        else:
+            self.orig_value = random.choice(self.d6_values)
+        self.rerolled_value = self.orig_value
         self.mod_value = self.orig_value
         self.rerolled = False
         
     def reroll(self):
-        self.orig_value = random.choice(self.d6_values)
-        self.mod_value = self.orig_value
+        self.rerolled_value = random.choice(self.d6_values)
+        self.mod_value = self.rerolled_value
         self.rerolled = True
         
     def apply_mod(self, modifier: int):
--- src/wh40k8edutils/roll_list.py
+++ src/wh40k8edutils/roll_list.py
@@ -2,9 +2,10 @@
 
 A list of rolls representing a batch of rolls
 
-@author: David
+@author: David Taylor
 '''
 
+import random
 from typing import List
 from wh40k8edutils.dieroll import DieRoll
 
@@ -15,18 +16,57 @@ class RollList:
 
 
     def __init__(self, num_rolls: int, passvalue: int,
-                 reroll_values: List[int] = None, mod: int = 0 ):
+                 reroll_values: List[int] = None, mod: int = 0,
+                 onealwaysfails: bool = False ):
         '''
         Constructor
         '''
-        if num_rolls <= 0:
+        if num_rolls <= 0: # can't roll zero dice
             raise ValueError('Invalid roll number', num_rolls)
         self.num_rolls = num_rolls
+        if passvalue <= 1: # No such thing as a natural 1+ roll
+            raise ValueError('Invalid pass value')
         self.passvalue = passvalue
         self.reroll_values = reroll_values
         self.mod = mod
+        self.onealwaysfails = onealwaysfails
         
+        random.seed()
         self.rolls = []
         for _ in range(self.num_rolls):
             self.rolls.append(DieRoll())
+            
+        self.do_rerolls()
+        self.apply_mod()
         
+    
+    def do_rerolls(self):
+        if self.reroll_values == None:
+            return
+        for item in self.rolls:
+            if item.orig_value in self.reroll_values and item.rerolled == False:
+                item.reroll()
+                
+    def apply_mod(self):
+        for item in self.rolls:
+            item.apply_mod(self.mod)
+            if self.onealwaysfails and item.orig_value == 1:
+                item.mod_value = 1
+
+    def count_passes(self) -> int:
+        result = 0
+        #self.do_rerolls()
+        #self.apply_mod()
+        for item in self.rolls:
+            if item.has_passed(self.passvalue):
+                result += 1
+        return result
+            
+    def count_failures(self) -> int:
+        result = 0
+        #self.do_rerolls()
+        #self.apply_mod()
+        for item in self.rolls:
+            if not item.has_passed(self.passvalue):
+                result += 1
+        return result
