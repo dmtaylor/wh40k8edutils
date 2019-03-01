60fa15340b934e6144f175c0e7432df1e353ba1f
David Taylor <dmtaylor2011@gmail.com>
2019-03-01 00:42:41 -0800

    add function type signature and value checks

--- src/wh40k8edutils/attackroll.py
+++ src/wh40k8edutils/attackroll.py
@@ -4,5 +4,32 @@ Created on Feb 28, 2019
 @author: David
 '''
 
-def roll(attacknumber: int, attackskil: int, strength: int, toughness):
-    pass
+
+def roll(attacknumber: int, attackskill: int, strength: int, toughness: int,
+         armor: int, ap: int, damage, rollmods: dict):
+    """ Primary rolling method. Takes the following args
+        attacknumber: number of attacks
+        attackskill: attackers skill (ws/bs)
+        strength/toughness: attackers S, defenders T
+        armor: defenders armor value
+        ap: ap value of attacks
+        damage: damage per attack, supports int, d3, and d6
+        rollmods: misc modifiers for attack. Rerolls, special rules, etc
+    """
+    
+    # number validation
+    if rollmods == None:
+        raise ValueError("rollmods is None")
+    
+    if attacknumber <= 0:
+        raise ValueError("Invalid attack number", attacknumber)
+    if attackskill <= 0 or attackskill >= 7:
+        raise ValueError("Invalid attack skill", attackskill)
+    if strength <= 0:
+        raise ValueError("Invalid strength", strength)
+    if toughness <= 0:
+        raise ValueError("Invalid toughness", toughness)
+    if armor <= 0:
+        raise ValueError("Invalid armor", armor)
+    if ap < 0:
+        raise ValueError("Invalid ap", ap)
